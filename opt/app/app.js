38ac5f097d1ebc4c2a01b4b65585a41994f8b6f7
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-02-26 22:51:26 +0200

    Utils method

--- opt/app/app.js
+++ opt/app/app.js
@@ -5,9 +5,19 @@ db.connect();
 User = require('app/modules/user');
 log = require('app/modules/logger')(module);
 
+util = require('util');
+
 function run() {
     let vasya = new User("Vasya");
     let petya = new User("Petya");
+
+    let obj = {
+        a: 10,
+        b: 15,
+        inspect: function() {
+            return "This is an object obj"
+        }
+    }
 
     vasya.hello(petya);
     petya.hello_arrow();
@@ -15,6 +25,7 @@ function run() {
     vasya.hello({name: "World"});
 
     log("Run is ok!");
+    console.log(util.inspect(obj));
 }
 
 if (module.parent) {
